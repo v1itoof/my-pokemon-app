608d4c75f3f7928f27e0837c73c1f56e2892021d
v1itoof <joaov1c@outlook.com>
2024-05-27 13:25:42 -0300

    TESTE

--- src/app/services/pokeapi.service.spec.ts
+++ src/app/services/pokeapi.service.spec.ts
@@ -1,16 +1,39 @@
+// pokeapi.service.spec.ts
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { PokeapiService } from './pokeapi.service';
 
 describe('PokeapiService', () => {
   let service: PokeapiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PokeapiService]
+    });
     service = TestBed.inject(PokeapiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return pokemon details', () => {
+    const pokemonId = 1;
+    const mockData = { id: pokemonId, name: 'Bulbasaur' };
+
+    service.getPokemonDetails(pokemonId).subscribe(data => {
+      expect(data).toEqual(mockData);
+    });
+
+    const req = httpMock.expectOne(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockData);
   });
 });
